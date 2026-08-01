428cdb99c6302bd87d9b2e11dc76b6659d06375d
alaminhossainrifat <rif420aat@gmail.com>
2026-08-02 00:29:23 +0600

    chore(capstone): setup PostgreSQL configuration with environment variables and ignore .env

---
+++ backend-ai-engineering/week08/widget-platform-backend/src/main/java/com/rifat/widget_platform_backend/WidgetPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rifat.widget_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetPlatformBackendApplication.class, args);
+	}
+
+}
