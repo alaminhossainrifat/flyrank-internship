63485f8d0d0714feacf976f5891aab40a65e4543
alaminhossainrifat <rif420aat@gmail.com>
2026-08-07 23:05:54 +0600

    chore: setup project dependencies and postgres database configuration

---
+++ backend-ai-engineering/week06/background_job/src/main/java/com/alaminhossainrifat/background_job/BackgroundJobApplication.java
@@ -0,0 +1,13 @@
+package com.alaminhossainrifat.background_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgroundJobApplication.class, args);
+	}
+
+}
